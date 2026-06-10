f0e8a1de279b45ef2600508db6007fe40dd6cb55
Yerukula Sathish <sathish630038@gmail.com>
2026-06-10 15:07:14 +0530

    chore: add model listing utility

---
+++ src/list_models.py
@@ -0,0 +1,9 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+for model in genai.list_models():
+    print(model.name)
